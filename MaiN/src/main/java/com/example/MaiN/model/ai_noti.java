d02e450708cb125fff1ea8564544c8ab097e4a22
Jaeyoung You <jaeyoung0703@naver.com>
2024-03-07 18:28:36 +0900

    학부 공지사항 Entity

---
+++ MaiN/src/main/java/com/example/MaiN/model/ai_noti.java
@@ -0,0 +1,42 @@
+package com.example.MaiN.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name="ai_noti")
+public class ai_noti {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "title", unique = true, nullable = false)
+    private String title;
+
+    @Column(length = 15, nullable = false)
+    private String link;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private boolean favorites;
+
+    @OneToMany(mappedBy = "aiNoti", fetch = FetchType.EAGER)
+    private Set<ai_noti_favorites> favoritesSet;
+
+    @Builder
+    public ai_noti(int id, String title, String link, LocalDate date, boolean favorites) {
+        this.id = Math.toIntExact(id);
+        this.title = title;
+        this.link = link;
+        this.date = date;
+        this.favorites = favorites;
+    }
+}
