139a2bac7dc5c77bae5b8c3a66dd6dcb8072dce1
Jaeyoung You <jaeyoung0703@naver.com>
2024-03-07 18:15:56 +0900

    펀시스템 관련 Controller

---
+++ MaiN/src/main/java/com/example/MaiN/controller/funsys_noti_favorites_Controller.java
@@ -0,0 +1,35 @@
+package com.example.MaiN.controller;
+
+import com.example.MaiN.dto.FunsysNotiDto;
+import com.example.MaiN.model.funsys_noti_favorites;
+import com.example.MaiN.service.FunsysNotiFavoritesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/funsysnoti/favorites")
+public class funsys_noti_favorites_Controller {
+
+    @Autowired
+    private FunsysNotiFavoritesService funsysNotiFavoritesService;
+
+    @PostMapping("/add/{studentId}/{funsysNotiId}")
+    public ResponseEntity<funsys_noti_favorites> addFavorite(@PathVariable String studentId, @PathVariable int funsysNotiId) {
+        funsys_noti_favorites favorite = funsysNotiFavoritesService.addFavorite(studentId, funsysNotiId);
+        return ResponseEntity.ok(favorite);
+    }
+    @DeleteMapping("delete/{studentId}/{funsysNotiId}")
+    public ResponseEntity<?> deleteFavorite(@PathVariable String studentId, @PathVariable int funsysNotiId) {
+        funsysNotiFavoritesService.deleteFavorite(studentId, funsysNotiId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/all/{studentId}")
+    public ResponseEntity<List<FunsysNotiDto>> getFunsysNotiWithFavorites(@PathVariable int studentId) {
+        List<FunsysNotiDto> funsysNotis = funsysNotiFavoritesService.getFunsysNotiWithFavorites(studentId);
+        return ResponseEntity.ok(funsysNotis);
+    }
+}
