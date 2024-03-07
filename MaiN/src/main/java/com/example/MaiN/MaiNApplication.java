bd544418d532da3c54215f3d8a8d42357dd08ac8
Jaeyoung You <jaeyoung0703@naver.com>
2024-03-07 18:42:56 +0900

    어플리케이션 세팅

---
+++ MaiN/src/main/java/com/example/MaiN/MaiNApplication.java
@@ -0,0 +1,13 @@
+package com.example.MaiN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaiNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaiNApplication.class, args);
+	}
+
+}
---
+++ MaiN/src/test/java/com/example/MaiN/MaiNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MaiN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaiNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
