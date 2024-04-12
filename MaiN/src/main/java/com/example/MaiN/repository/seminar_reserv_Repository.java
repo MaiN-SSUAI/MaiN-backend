3e94824c5265851edce22cdb9080bd8f6a5a6697
yeonjae037 <jyj030307@gmail.com>
2024-04-12 17:05:15 +0900

    [#21] 주당 2시간 제한 + 다음 달 예약 제한 구현

--- MaiN/src/main/java/com/example/MaiN/repository/seminar_reserv_Repository.java
+++ MaiN/src/main/java/com/example/MaiN/repository/seminar_reserv_Repository.java
@@ -1,8 +1,16 @@
 package com.example.MaiN.repository;
 
 import com.example.MaiN.entity.EventEntity;
+import com.example.MaiN.entity.users;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface seminar_reserv_Repository extends CrudRepository<EventEntity,Integer> {
     EventEntity findByeventid(String eventid);
+    @Query("SELECT e FROM EventEntity e WHERE e.student_id = :student_id")
+    List<EventEntity> findByStudent_id(@Param("student_id") String student_id);
+
 }
