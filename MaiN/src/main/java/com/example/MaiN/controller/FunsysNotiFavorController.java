0defffaf21ed10f9f4c6aa23e1bc5e2a51b03f2f
Jaeyoung You <jaeyoung0703@naver.com>
2024-05-02 14:48:57 +0900

    Swagger ui 설정 추가

--- MaiN/src/main/java/com/example/MaiN/controller/FunsysNotiFavorController.java
+++ MaiN/src/main/java/com/example/MaiN/controller/FunsysNotiFavorController.java
@@ -3,6 +3,8 @@ package com.example.MaiN.controller;
 import com.example.MaiN.dto.FunsysNotiDto;
 import com.example.MaiN.entity.FunsysNotiFavor;
 import com.example.MaiN.service.FunsysNotiFavorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Tag(name="FunsysNoti-Favorite-Controller",description = "펀시스템 북마크 관련 API")
 @RequestMapping("/funsysnoti/favorites")
 public class FunsysNotiFavorController {
 
@@ -17,17 +20,20 @@ public class FunsysNotiFavorController {
     private FunsysNotiFavorService funsysNotiFavorService;
 
     @PostMapping("/add/{studentId}/{funsysNotiId}")
+    @Operation(summary = "북마크 추가")
     public ResponseEntity<FunsysNotiFavor> addFavorite(@PathVariable String studentId, @PathVariable int funsysNotiId) {
         FunsysNotiFavor favorite = funsysNotiFavorService.addFavorite(studentId, funsysNotiId);
         return ResponseEntity.ok(favorite);
     }
     @DeleteMapping("delete/{studentId}/{funsysNotiId}")
+    @Operation(summary = "북마크 삭제")
     public ResponseEntity<?> deleteFavorite(@PathVariable String studentId, @PathVariable int funsysNotiId) {
         funsysNotiFavorService.deleteFavorite(studentId, funsysNotiId);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/all/{studentId}")
+    @Operation(summary = "모든 북마크 보기")
     public ResponseEntity<List<FunsysNotiDto>> getFunsysNotiWithFavorites(@PathVariable int studentId) {
         List<FunsysNotiDto> funsysNotis = funsysNotiFavorService.getFunsysNotiWithFavorites(studentId);
         return ResponseEntity.ok(funsysNotis);
