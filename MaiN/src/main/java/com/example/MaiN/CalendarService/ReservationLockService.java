9bb1925b764f87f113801c19a43e912e39f0a026
Supermoon-JH <dknl133@gmail.com>
2024-06-26 16:56:30 +0900

    [#123] 세미나실 동시성 문제 해결 #3

--- MaiN/src/main/java/com/example/MaiN/CalendarService/ReservationLockService.java
+++ MaiN/src/main/java/com/example/MaiN/CalendarService/ReservationLockService.java
@@ -1,8 +1,10 @@
 package com.example.MaiN.CalendarService;
 
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.ConcurrentHashMap;
-
 
 @Service
 public class ReservationLockService {
@@ -10,7 +12,7 @@ public class ReservationLockService {
     private static final ConcurrentHashMap<String, Object> locks = new ConcurrentHashMap<>();
 
     public static <T> T executeWithLock(String startTime, String endTime, LockCallback<T> callback) throws Exception {
-        String lockKey = startTime + ":" + endTime;
+        String lockKey = createLockKey(startTime, endTime);
         Object lock = locks.computeIfAbsent(lockKey, k -> new Object());
 
         synchronized (lock) {
@@ -20,6 +22,19 @@ public class ReservationLockService {
                 locks.remove(lockKey);
             }
         }
+    }
+
+    private static String createLockKey(String startTime, String endTime) {
+        ZonedDateTime start = ZonedDateTime.parse(startTime);
+        ZonedDateTime end = ZonedDateTime.parse(endTime);
+
+        StringBuilder keyBuilder = new StringBuilder();
+        ZonedDateTime current = start;
+        while (current.isBefore(end)) {
+            keyBuilder.append(current.format(DateTimeFormatter.ofPattern("yyyyMMddHHmm"))).append(":");
+            current = current.plusMinutes(10);
+        }
+        return keyBuilder.toString();
     }
 
     @FunctionalInterface
