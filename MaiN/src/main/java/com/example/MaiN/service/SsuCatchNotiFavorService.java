c0e1b1dd5abd5af7a9f819eaefd807095dc663ef
yeonjae037 <jyj030307@gmail.com>
2024-06-27 16:43:46 +0900

    feat: IOS 요구 api 구현

--- MaiN/src/main/java/com/example/MaiN/service/SsuCatchNotiFavorService.java
+++ MaiN/src/main/java/com/example/MaiN/service/SsuCatchNotiFavorService.java
@@ -79,4 +79,16 @@ public class SsuCatchNotiFavorService {
 
         return query.getResultList();
     }
+
+    public List<SsuCatchNotiDto> getSsuCatchWithFavoritesDev(int studentNo) {
+        String jpql = "SELECT new com.example.MaiN.dto.SsuCatchNotiDto(an.id, an.title, an.link, an.progress, an.category, an.date, CASE WHEN af IS NOT NULL THEN true ELSE false END) " +
+                "FROM SsuCatchNoti an LEFT JOIN an.favoritesSet af " +
+                "WITH af.studentNo.studentNo = :studentNo " +
+                "ORDER BY an.date DESC";
+
+        TypedQuery<SsuCatchNotiDto> query = entityManager.createQuery(jpql, SsuCatchNotiDto.class);
+        query.setParameter("studentNo", studentNo);
+
+        return query.getResultList();
+    }
 }
