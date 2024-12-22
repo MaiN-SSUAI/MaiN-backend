56215128a40503954b2d749510b4e1937431e41c
yeonjae037 <jyj030307@gmail.com>
2024-12-22 23:15:51 +0900

    fix: PostConstruct를 이용한 기본 TimeZone 설정

--- MaiN/src/main/java/com/example/MaiN/MaiNApplication.java
+++ MaiN/src/main/java/com/example/MaiN/MaiNApplication.java
@@ -3,11 +3,20 @@ package com.example.MaiN;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.Locale;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MaiNApplication {
+
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		Locale.setDefault(Locale.KOREA);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaiNApplication.class, args);
 	}
-
 }
